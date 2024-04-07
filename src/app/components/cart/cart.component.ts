9b6ae42266a805186bbefb39457042c5fc57d479
Mousa <54138093+Mousa-Ahmed98@users.noreply.github.com>
2024-04-07 13:02:18 +0200

    looping over cart items

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CartService } from '../../services/cart.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
